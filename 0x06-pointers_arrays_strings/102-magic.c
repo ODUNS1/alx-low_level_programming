0707a3e12742fd611abbc5336ab83e3555faa717
ODUNS1 <ohafis459@gmail.com>
2023-03-02 01:04:00 +0000

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,7 +10,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 /*
- * main - head of the code
+ * main - check the code
  * write your line of code here...
  * Remember:
  * - you are not allowed to use a
